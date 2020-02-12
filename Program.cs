7acc2e54c3aac496c5e0c2c0c15b7e2f8e06d8fa
Dingus Corp <dingus@gmail.com>
2020-02-12 14:33:36 -0700

    Class Exercise 4

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharo_Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
