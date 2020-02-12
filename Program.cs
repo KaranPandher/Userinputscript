ff939624a511e7ae5b14b371f3033b81ab022fb0
Dingus Corp <dingus@gmail.com>
2020-02-12 15:25:50 -0700

    Multiple methods

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace C_Sharo_Exercise_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter User Input: ");
+            string input = Console.ReadLine(); // Store user input 
+            Console.WriteLine(":( " + input + ":)");
+
+            string output = GetOutput();
+            output = Smileatthebeginning(output);
+            output = Smileatend(output);
+            Console.WriteLine(output);
         }
+            static string GetOutput()
+            {
+                Console.WriteLine("Enter Input: ");
+                return Console.ReadLine();
+            }
+
+            static string Smileatthebeginning( string output)
+            {
+               output = output + " :) ";
+               return output; 
+            }
+
+            static string Smileatend( string output)
+            {    output = " :( " + output;
+                return output; 
+            }
+
+        
     }
 }
